f330ad2d6123a4310ce31722733e3c22e8f9c869
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-03-28 17:58:07 -0300

    Update endereco.py

--- endereco.py
+++ endereco.py
@@ -1,6 +1,6 @@
-""" Módulo mover_arquivos.py
+""" Módulo endereco.py
 Programa: Projeto Final
-Descrição: Este módulo lista as pastas a serem organizadas
+Descrição: Este módulo solicita o diretorio desejado, para torná-lo o diretório atual. Então, lista os arquivos que estão nele, para posterior organização.
 Versão: 0.0.1
 Data: 22/03/2025
 """
@@ -10,5 +10,4 @@ import os
 def endereco() -> str:
     diretorio_analisado = input("Digite o endereço da pasta que deseja organizar: ") # o usuario cola o endereço da pasta a ser organizada
     os.chdir(f"{diretorio_analisado}") # aqui o endereço da pasta informada pelo usuario se torna a pasta "atual" trabalhada
-    lista_de_arquivos: list[str]
 
